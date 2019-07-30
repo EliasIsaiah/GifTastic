2f3d561d23070428f0d9ba56fcb29be2d3612086
Elias Roussos <eliasIsaiah@outlook.com>
2019-07-30 13:34:37 -0400

    -button click builds new query url

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,26 +5,30 @@ $(document).ready(function () {
     let giphyJSON = null;
     let apiKey = "Uhn8mVWdgQYbSvuPCeCAC6ybnhmN7JPg";
     let topics = ["Peggy Hill", "Bobby Hill", "Hank Hill", "Boomhauer", "Luanne Platter", "Bill Dauterive"];
-    let queryURL = "";
+    let queryURL = `https://api.giphy.com/v1/gifs/search?&api_key=${apiKey}`;
     let ratings = ["g", "pg", "pg-13", "r"];
     let gifLimit = 5;
 
-    function buildAppDOM() {
+    function buildDefaultDOM() {
         topics.forEach(starterButton => {
             let newButton = $("<div>")
-            .addClass("btn btn-primary")
-            .attr("data-value", starterButton)
-            .text(starterButton)
-            .on("click", buttonClickQuery);
+                .addClass("btn btn-primary")
+                .attr("data-value", starterButton)
+                .text(starterButton)
+                .on("click", buttonClickQuery);
 
             $("div.buttonsDiv").append(newButton);
         })
     }
 
-    queryURL = `https://api.giphy.com/v1/gifs/search?q=${topics[0]}&rating=${ratings[0]}&limit=${gifLimit}&api_key=${apiKey}`;
 
     function buttonClickQuery() {
         console.log("nothing here yet!");
+        _this = $(this);
+        console.log(_this);
+        let queryAdd = encodeURIComponent($(this).attr("data-value"));
+        let newQueryURL = queryURL + "&q=" + queryAdd;
+        console.log(newQueryURL);
     }
 
     function testGet(route) {
@@ -34,6 +38,6 @@ $(document).ready(function () {
     }
 
     testGet(queryURL);
-    buildAppDOM();
+    buildDefaultDOM();
 
 });
