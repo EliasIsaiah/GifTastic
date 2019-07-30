b09e6f67191f8e3ddb9862a7be5978a94e87edc0
Elias Roussos <eliasIsaiah@outlook.com>
2019-07-30 12:56:16 -0400

    -buttons are being dynamically added to the page -working click events implemented on buttons -more boostrap empty elements added to the page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    const $container = $("div.container");
+
     let giphyJSON = null;
     let apiKey = "Uhn8mVWdgQYbSvuPCeCAC6ybnhmN7JPg";
     let topics = ["Peggy Hill", "Bobby Hill", "Hank Hill", "Boomhauer", "Luanne Platter", "Bill Dauterive"];
@@ -7,18 +9,31 @@ $(document).ready(function () {
     let ratings = ["g", "pg", "pg-13", "r"];
     let gifLimit = 5;
 
+    function buildAppDOM() {
+        topics.forEach(starterButton => {
+            let newButton = $("<div>")
+            .addClass("btn btn-primary")
+            .attr("data-value", starterButton)
+            .text(starterButton)
+            .on("click", buttonClickQuery);
+
+            $("div.buttonsDiv").append(newButton);
+        })
+    }
 
     queryURL = `https://api.giphy.com/v1/gifs/search?q=${topics[0]}&rating=${ratings[0]}&limit=${gifLimit}&api_key=${apiKey}`;
 
+    function buttonClickQuery() {
+        console.log("nothing here yet!");
+    }
 
     function testGet(route) {
-       return $.get(route).then(function (apiData) {
+        return $.get(route).then(function (apiData) {
             giphyJSON = apiData;
         })
     }
 
     testGet(queryURL);
-
+    buildAppDOM();
 
 });
-
