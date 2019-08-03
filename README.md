4a3840e03cc6800613060a248b18a8cd9fd28024
Elias Roussos <eliasIsaiah@outlook.com>
2019-08-02 21:41:26 -0400

    -app is fully functional, but there is currently nothing additional implimented beyond what the homework has prescribed.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GifTastic
+
+### GifTastic
+
+This is a relatively simple web page that loads a default set of search topics in the form of buttons. Upon clicking each button the query that corresponds with that button is sent to Giphy which then returns via the Giphy API the top ten gifs associated with that query.
+
+Besides the default query options, the user has the option of writing their own query strings and publishing them as buttons that they can then select to see the corresponding gifs.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,14 @@ $(document).ready(function () {
 
     let giphyJSON = null;
     let apiKey = "Uhn8mVWdgQYbSvuPCeCAC6ybnhmN7JPg";
-    let topics = ["Peggy Hill", "Bobby Hill", "Hank Hill", "Boomhauer", "Luanne Platter", "Bill Dauterive"];
+    let topics = ["Peggy Hill", "Bobby Hill", "Hank Hill", "Luanne Platter", "Bill Dauterive"];
     let queryURL = `https://api.giphy.com/v1/gifs/search?&api_key=${apiKey}`;
     let ratings = ["g", "pg", "pg-13", "r"];
+    let selectedRating = "";
     let gifLimit = 5;
 
-    function buildDefaultDOM() {
+    function buildDefaultButtonsDOM() {
+        console.log("entered buildButtonDOM");
         topics.forEach(defaultButton => {
             let newButton = $("<div>")
                 .addClass("btn btn-primary m-1 gifButton")
@@ -21,9 +23,21 @@ $(document).ready(function () {
         })
     }
 
+    //overloaded to accept userInput parameter
+
+    function buildNewButtonDOM(userInput) {
+        let newButton = $("<div>")
+            .addClass("btn btn-primary m-1 gifButton")
+            .attr("data-value", userInput)
+            .text(userInput)
+            .on("click", buttonClickQuery);
+        $("div.buttonsDiv").append(newButton);
+    }
+
+
     function buttonClickQuery() {
         let queryAdd = encodeURIComponent($(this).attr("data-value"));
-        let newQueryURL = queryURL + "&q=" + queryAdd + "&limit=" + gifLimit;
+        let newQueryURL = `${queryURL}&q=${queryAdd}&limit=${gifLimit}&rating=${selectedRating}`;
 
         buildGifsDOM(newQueryURL);
     }
@@ -31,11 +45,15 @@ $(document).ready(function () {
     function buildGifsDOM(url) {
         $.get(url).then(gifObject => {
             gifObject.data.forEach(element => {
-                $newImg = $("<img>")
+                let $newDiv = $("<div>");
+                let $newP = $("<p>");
+                let $newImg = $("<img>")
                     .addClass("gifImg")
-                    .attr("src", element.images.downsized_still.url)
-                    .attr("data-animatedUrl", element.images.downsized.url)
-                    .attr("data-stillUrl", element.images.downsized_still.url)
+                    .attr({
+                        src: element.images.downsized_still.url,
+                        "data-animatedUrl": element.images.downsized.url,
+                        "data-stillUrl": element.images.downsized_still.url
+                    })
                     .on("mouseenter", function () {
                         $(this).attr("src", $(this).attr("data-animatedUrl"));
                     })
@@ -47,11 +65,32 @@ $(document).ready(function () {
                         "max-width": "100%",
                         "margin": ".2rem",
                     });
-                $("div.gifs").prepend($newImg);
+                $newP.text(`Rating : ${element.rating}`);
+                $newDiv.append($newImg);
+                $newDiv.append($newP);
+                $("div.gifs").prepend($newDiv);
             })
         })
     }
 
-    buildDefaultDOM();
+    $(window).keydown(function (event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            if ($("input.userInput").val()) {
+                buildNewButtonDOM($("input.userInput").val());
+            } else
+                return false;
+        }
+    })
+
+    // $("input.userInput").on("")
+
+    $("button.userInputSubmit").on("click", (event) => {
+        event.preventDefault();
+        buildNewButtonDOM($("input.userInput").val());
+        // console.log(`user input: ${$("input.userInput").val()}`)
+    });
+
+    buildDefaultButtonsDOM();
 
 });
